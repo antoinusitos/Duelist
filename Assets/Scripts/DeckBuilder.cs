4f0f465ba9653baffb43cdce7e0e22d868a8b977
Antoine <antoine@warpfrog.com>
2020-08-18 18:00:52 +0200

    deck building
    adding deck building

---
+++ Assets/Scripts/DeckBuilder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DeckBuilder : MonoBehaviour
+{
+    [SerializeField]
+    private Slider mySlider = null;
+
+    private int myCurrentDeckValue = 0;
+
+    private Card[] myDeck = null;
+
+    private void Start()
+    {
+        myDeck = new Card[10];
+    }
+}
